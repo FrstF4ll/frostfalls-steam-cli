e00222d89a247d0d2b065acc8b59f3f954a0b04f
FrstF4ll <hydralixe@gmail.com>
2026-06-19 11:54:13 +0200

    refactor: directly verify dependencies within the function.
    - Delete dedicated function for steam client and steam cmd.

--- frostfall/validator.py
+++ frostfall/validator.py
@@ -1,23 +1,14 @@
 import shutil
 
 
-def check_steam_client():
-    if shutil.which("steam"):
-        answer = "steam client is installed"
-    else:
-        answer = "steam client is not installed"
-
-    return answer
-
-
-def check_steamcmd():
-    if shutil.which("steamcmd"):
-        answer = "steamcmd is installed"
-    else:
-        answer = "steamcmd is not installed"
-
-    return answer
-
-
 def dependencies():
-    return check_steamcmd() + " " + check_steam_client()
+    steam_client = shutil.which("steam")
+    steam_cmd = shutil.which("steamcmd")
+    success = "All dependencies installed"
+
+    if steam_client and steam_cmd:
+        return success
+    elif steam_client:
+        return "Steamcmd is missing, please install steamcmd"
+    elif steam_cmd:
+        return "Steam client is missing, please install steam client"
