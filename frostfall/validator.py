06289e187ec9335d0aaabbe930253add9e16e47f
FrstF4ll <hydralixe@gmail.com>
2026-06-19 12:53:00 +0200

    refactor: build dependencies in a match/case structure

--- frostfall/validator.py
+++ frostfall/validator.py
@@ -6,9 +6,15 @@ def dependencies():
     steam_cmd = shutil.which("steamcmd")
     success = "All dependencies installed"
 
-    if steam_client and steam_cmd:
-        return success
-    elif steam_client:
-        return "Steamcmd is missing, please install steamcmd"
-    elif steam_cmd:
-        return "Steam client is missing, please install steam client"
+    match (steam_client, steam_cmd):
+        case (True, True):
+            return success
+        case (True, False):
+            return "Steamcmd is missing, please install steamcmd"
+        case (False, True):
+            return "Steam client is missing, please install steam client"
+        case (False, False):
+            return """Missing dependencies, please install :
+            - Steamcmd
+            - Steam client (steam)
+            """
